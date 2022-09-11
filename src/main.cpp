3c7e71bf30b23b5f283647c2d89fe092ca47b5c5
thuibregtse <tomhuib@gmail.com>
2022-09-10 19:36:10 -0700

    Got it working with D3>Tx D4>Rx

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <Arduino.h>
-
 // Test program for the MD_YX5300 library
 //
 // Menu driven interface using the Serial Monitor to test individual functions.
@@ -7,50 +5,94 @@
 // Dependencies
 // MD_cmdProcessor library found at https://github.com/MajicDesigns/MD_cmdProcessor
 //
-
-
-#define USE_SOFTWARESERIAL 1   ///< Set to 1 to use SoftwareSerial library, 0 for native serial port
+#include <Adafruit_NeoPixel.h>
 #include <MD_YX5300.h>
 #include <MD_cmdProcessor.h>
 #include <SoftwareSerial.h>
 
+#define ConsoleBaudRate 115200
 
+#define NeoPixelPin D5
+#define NumberOfNeoPixels 24  //  This is for the 24-LED neopixel ring.  Adjust for what you have for Neopixels
+#define USE_SOFTWARESERIAL 1   ///< Set to 1 to use SoftwareSerial library, 0 for native serial port
 #define Console   Serial   // command processor input/output stream
-
-
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 #define CMD(s) { Console.print(F("\n>")); Console.print(F(s)); Console.print(F(" ")); }
-// Connections for serial interface to the YX5300 module
-const uint8_t MP3_RX = 0;    // "D3" connect to TX of MP3 Player module
-const uint8_t MP3_TX = 2;    // "D4" connect to RX of MP3 Player module
 
-//SoftwareSerial  MP3Stream(ARDUINO_RX, ARDUINO_TX);  // MP3 player serial stream for comms
-SoftwareSerial  softserial(MP3_RX, MP3_TX);  // MP3 player serial stream for comms
-//SoftwareSerial  softserial(0, 2);  // MP3 player serial stream for comms
+// Neopixel color for showing hour and 1/2 hours on the ring.  Adjust for Neopixel strip
+int HourLedColor[] = {50, 50, 50, 100};  // Should pre-define some static colors
 
-
-
-// Define YX5300 global variables
-MD_YX5300 mp3(softserial);
-bool bUseCallback = true; // use callbacks?
-bool bUseSynch = false;   // use synchronous? 
-
-// Begin cuckoo clock mods
-bool isTrackFinished = true;
-
+int neoPixelArray[NumberOfNeoPixels][4];  // RGBS array so we can set all NeoPixel and write all at once.
 int chimeCounter = 0;
 
 int oldHours = -1;
 int oldMinutes = -1;
 int oldSeconds = -1;
 
-int Hours = 6;
+int Hours = 4;
 int Minutes = 30;
 int Seconds = -1;
 
 
+// RGB strip has connected to NeoPixelPin.  There are other, more real-time modes, but we're saving our interrupts and timers for now.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NumberOfNeoPixels, NeoPixelPin, NEO_GRB + NEO_KHZ800);
 
 
+// Connections for soft serial interface to the YX5300 module.  Note these are "D" (data) pin numbers, not physical pin numbers
+const uint8_t MP3_RX = 0;    // connect to TX of MP3 Player module
+const uint8_t MP3_TX = 2;    // connect to RX of MP3 Player module
+
+//const uint8_t MP3_RX = 2;    // connect to TX of MP3 Player module
+//const uint8_t MP3_TX = 0;    // connect to RX of MP3 Player module
+
+SoftwareSerial  softserial(MP3_RX, MP3_TX);  // MP3 player serial stream for MP3 comms
+MD_YX5300 mp3(softserial); //Create the  YX5300 object for managing audio
+bool bUseCallback = true; // use callbacks
+bool bUseSynch = false;   // not synchronous
+
+// MP3 indicates when a track is done by sending a token over serial.  We'll watch for that on an interrupt.  Assume at start nothing is playi9ng.
+bool isTrackFinished = true;
+
+
+
+void readRTC() {
+  ;  // Read the real-time clock, put results in Hours, Minutes, Seconds
+  // This gets called during the main loop.
+  // Results determine if chime or update Neopixel ring.
+}
+
+//simple function which takes values for the red, green and blue led and also
+//a delay
+void setColor(int led, int redValue, int greenValue, int blueValue, int delayValue)
+{
+  pixels.setPixelColor(led, pixels.Color(redValue, greenValue, blueValue));
+  pixels.show();
+  //delay(delayValue);
+}
+
+void showTimeOnNeopixelRing() {
+  //  Update the  neopixel ring
+  //  Values in Hours + minutes determine how many LEDs are lit, and how bright
+  //  0, 2, 4, and other even-numbered LEDs represent an hour and are fully lit
+  //  Odd-numberred LEDs represent the half-hour, and my be less fully lit or a different color
+  //  This means noon and midnight would be dark....maybe have to re-think that.
+  int i = 0;
+
+  while ( i <= Hours ) {
+    setColor(((i - 1) * 2), HourLedColor[0], HourLedColor[1], HourLedColor[2], HourLedColor[3]);
+    i++;
+  }
+  //if (Minutes >= 30)  // Dim light for each half hour.  Might be able to do this with just luminance.
+  //  setColor((((Hours - 1) * 2) + 1), (HourLedColor[0] / 2), (HourLedColor[1] / 2), (HourLedColor[2] / 2), (HourLedColor[3] / 2));
+}
+
+void turnPixelsSolid(int red, int green, int blue) {
+  int led;
+  for (led = 0; led <= NumberOfNeoPixels; led++)
+  {
+    setColor(led, red, green, blue, 10);
+  }
+}
 
 void cbResponse(const MD_YX5300::cbData *status)
 // Used to process device responses either as a library callback function
@@ -63,25 +105,27 @@ void cbResponse(const MD_YX5300::cbData *status)
 
   switch (status->code)
   {
-  case MD_YX5300::STS_OK:         Console.print(F("STS_OK"));         break;
-  case MD_YX5300::STS_TIMEOUT:    Console.print(F("STS_TIMEOUT"));    break;
-  case MD_YX5300::STS_VERSION:    Console.print(F("STS_VERSION"));    break;
-  case MD_YX5300::STS_CHECKSUM:   Console.print(F("STS_CHECKSUM"));    break;
-  case MD_YX5300::STS_TF_INSERT:  Console.print(F("STS_TF_INSERT"));  break;
-  case MD_YX5300::STS_TF_REMOVE:  Console.print(F("STS_TF_REMOVE"));  break;
-  case MD_YX5300::STS_ERR_FILE:   Console.print(F("STS_ERR_FILE"));   break;
-  case MD_YX5300::STS_ACK_OK:     Console.print(F("STS_ACK_OK"));     break;
-  //  TOM MOD HERE:   set the global isTrackFinished so the loop can detect end of that play
-  case MD_YX5300::STS_FILE_END:   {Console.print(F("STS_FILE_END")); isTrackFinished = true;} break;
-  case MD_YX5300::STS_INIT:       Console.print(F("STS_INIT"));       break;
-  case MD_YX5300::STS_STATUS:     Console.print(F("STS_STATUS"));     break;
-  case MD_YX5300::STS_EQUALIZER:  Console.print(F("STS_EQUALIZER"));  break;
-  case MD_YX5300::STS_VOLUME:     Console.print(F("STS_VOLUME"));     break;
-  case MD_YX5300::STS_TOT_FILES:  Console.print(F("STS_TOT_FILES"));  break;
-  case MD_YX5300::STS_PLAYING:    Console.print(F("STS_PLAYING"));    break;
-  case MD_YX5300::STS_FLDR_FILES: Console.print(F("STS_FLDR_FILES")); break;
-  case MD_YX5300::STS_TOT_FLDR:   Console.print(F("STS_TOT_FLDR"));   break;
-  default: Console.print(F("STS_??? 0x")); Console.print(status->code, HEX); break;
+    case MD_YX5300::STS_OK:         Console.print(F("STS_OK"));         break;
+    case MD_YX5300::STS_TIMEOUT:    Console.print(F("STS_TIMEOUT"));    break;
+    case MD_YX5300::STS_VERSION:    Console.print(F("STS_VERSION"));    break;
+    case MD_YX5300::STS_CHECKSUM:   Console.print(F("STS_CHECKSUM"));    break;
+    case MD_YX5300::STS_TF_INSERT:  Console.print(F("STS_TF_INSERT"));  break;
+    case MD_YX5300::STS_TF_REMOVE:  Console.print(F("STS_TF_REMOVE"));  break;
+    case MD_YX5300::STS_ERR_FILE:   Console.print(F("STS_ERR_FILE"));   break;
+    case MD_YX5300::STS_ACK_OK:     Console.print(F("STS_ACK_OK"));     break;
+    case MD_YX5300::STS_FILE_END:   {
+        Console.print(F("STS_FILE_END"));
+        isTrackFinished = true;  // Tom modification to say track is finished, perhaps play next chime if needed
+      } break;
+    case MD_YX5300::STS_INIT:       Console.print(F("STS_INIT"));       break;
+    case MD_YX5300::STS_STATUS:     Console.print(F("STS_STATUS"));     break;
+    case MD_YX5300::STS_EQUALIZER:  Console.print(F("STS_EQUALIZER"));  break;
+    case MD_YX5300::STS_VOLUME:     Console.print(F("STS_VOLUME"));     break;
+    case MD_YX5300::STS_TOT_FILES:  Console.print(F("STS_TOT_FILES"));  break;
+    case MD_YX5300::STS_PLAYING:    Console.print(F("STS_PLAYING"));    break;
+    case MD_YX5300::STS_FLDR_FILES: Console.print(F("STS_FLDR_FILES")); break;
+    case MD_YX5300::STS_TOT_FLDR:   Console.print(F("STS_TOT_FLDR"));   break;
+    default: Console.print(F("STS_??? 0x")); Console.print(status->code, HEX); break;
   }
 
   Console.print(F(", 0x"));
@@ -105,28 +149,49 @@ void setSynchMode(bool b)
 }
 
 
+
 char * getNum(char *cp, uint32_t &v, uint8_t base = 10)
 {
   char* rp;
 
   v = strtoul(cp, &rp, base);
 
-  return(rp);
+  return (rp);
 }
 
 // Command processor handlers
 void handlerHelp(char* param);
 
-void handlerP_bang(char* param) { CMD("Play Start"); mp3.playStart(); cbResponse(mp3.getStatus()); }
-void handlerPP(char* param)     { CMD("Play Pause"); mp3.playPause(); cbResponse(mp3.getStatus()); }
-void handlerPZ(char* param)     { CMD("Play Stop");  mp3.playStop();  cbResponse(mp3.getStatus()); }
-void handlerP_gt(char* param)   { CMD("Play Next");  mp3.playNext();  cbResponse(mp3.getStatus()); }
-void handlerP_lt(char* param)   { CMD("Play Prev");  mp3.playPrev();  cbResponse(mp3.getStatus()); }
+void handlerP_bang(char* param) {
+  CMD("Play Start");
+  mp3.playStart();
+  cbResponse(mp3.getStatus());
+}
+void handlerPP(char* param)     {
+  CMD("Play Pause");
+  mp3.playPause();
+  cbResponse(mp3.getStatus());
+}
+void handlerPZ(char* param)     {
+  CMD("Play Stop");
+  mp3.playStop();
+  cbResponse(mp3.getStatus());
+}
+void handlerP_gt(char* param)   {
+  CMD("Play Next");
+  mp3.playNext();
+  cbResponse(mp3.getStatus());
+}
+void handlerP_lt(char* param)   {
+  CMD("Play Prev");
+  mp3.playPrev();
+  cbResponse(mp3.getStatus());
+}
 
 void handlerP(char* param)
 {
   uint32_t t;
-  
+
   getNum(param, t);
   CMD("Play Track");
   Console.print(t);
@@ -162,7 +227,7 @@ void handlerPF(char* param)
 void handlerPX(char* param)
 {
   uint32_t fldr;
-  
+
   getNum(param, fldr);
   CMD("Play Shuffle Folder");
   Console.print(fldr);
@@ -185,7 +250,7 @@ void handlerPR(char* param)
 void handlerVM(char *param)
 {
   uint32_t cmd;
-  
+
   getNum(param, cmd);
   CMD("Volume Enable");
   Console.print(cmd);
@@ -196,28 +261,60 @@ void handlerVM(char *param)
 void handlerV(char *param)
 {
   uint32_t v;
-  
+
   getNum(param, v);
-  CMD("Volume"); 
+  CMD("Volume");
   Console.print(v);
-  mp3.volume(v); 
+  mp3.volume(v);
   cbResponse(mp3.getStatus());
 }
 
-void handlerV_plus(char* param)  { CMD("Volume Up");   mp3.volumeInc(); cbResponse(mp3.getStatus()); }
-void handlerV_minus(char* param) { CMD("Volume Down"); mp3.volumeDec(); cbResponse(mp3.getStatus()); }
+void handlerV_plus(char* param)  {
+  CMD("Volume Up");
+  mp3.volumeInc();
+  cbResponse(mp3.getStatus());
+}
+void handlerV_minus(char* param) {
+  CMD("Volume Down");
+  mp3.volumeDec();
+  cbResponse(mp3.getStatus());
+}
 
-void handlerQE(char* param) { CMD("Query Equalizer");    mp3.queryEqualizer();   cbResponse(mp3.getStatus()); }
-void handlerQF(char* param) { CMD("Query File");         mp3.queryFile();        cbResponse(mp3.getStatus()); }
-void handlerQS(char* param) { CMD("Query Status");       mp3.queryStatus();      cbResponse(mp3.getStatus()); }
-void handlerQV(char* param) { CMD("Query Volume");       mp3.queryVolume();      cbResponse(mp3.getStatus()); }
-void handlerQX(char* param) { CMD("Query Folder Count"); mp3.queryFolderCount(); cbResponse(mp3.getStatus()); }
-void handlerQY(char* param) { CMD("Query Tracks Count"); mp3.queryFilesCount();  cbResponse(mp3.getStatus()); }
+void handlerQE(char* param) {
+  CMD("Query Equalizer");
+  mp3.queryEqualizer();
+  cbResponse(mp3.getStatus());
+}
+void handlerQF(char* param) {
+  CMD("Query File");
+  mp3.queryFile();
+  cbResponse(mp3.getStatus());
+}
+void handlerQS(char* param) {
+  CMD("Query Status");
+  mp3.queryStatus();
+  cbResponse(mp3.getStatus());
+}
+void handlerQV(char* param) {
+  CMD("Query Volume");
+  mp3.queryVolume();
+  cbResponse(mp3.getStatus());
+}
+void handlerQX(char* param) {
+  CMD("Query Folder Count");
+  mp3.queryFolderCount();
+  cbResponse(mp3.getStatus());
+}
+void handlerQY(char* param) {
+  CMD("Query Tracks Count");
+  mp3.queryFilesCount();
+  cbResponse(mp3.getStatus());
+}
 
 void handlerQZ(char* param)
 {
   uint32_t fldr;
-  
+
   getNum(param, fldr);
   CMD("Query Folder Files Count");
   Console.print(fldr);
@@ -225,9 +322,21 @@ void handlerQZ(char* param)
   cbResponse(mp3.getStatus());
 }
 
-void handlerS(char *param) { CMD("Sleep");   mp3.sleep();  cbResponse(mp3.getStatus()); }
-void handlerW(char *param) { CMD("Wake up"); mp3.wakeUp(); cbResponse(mp3.getStatus()); }
-void handlerZ(char *param) { CMD("Reset");   mp3.reset();  cbResponse(mp3.getStatus()); }
+void handlerS(char *param) {
+  CMD("Sleep");
+  mp3.sleep();
+  cbResponse(mp3.getStatus());
+}
+void handlerW(char *param) {
+  CMD("Wake up");
+  mp3.wakeUp();
+  cbResponse(mp3.getStatus());
+}
+void handlerZ(char *param) {
+  CMD("Reset");
+  mp3.reset();
+  cbResponse(mp3.getStatus());
+}
 
 void handlerE(char *param)
 {
@@ -265,7 +374,7 @@ void handlerR(char* param)
 void handlerY(char *param)
 {
   uint32_t cmd;
-  
+
   getNum(param, cmd);
   setSynchMode(cmd != 0);
 }
@@ -315,6 +424,7 @@ const MD_cmdProcessor::cmdItem_t PROGMEM cmdTable[] =
 
 MD_cmdProcessor CP(Console, cmdTable, ARRAY_SIZE(cmdTable));
 
+
 // handler functions
 void handlerHelp(char* param)
 {
@@ -325,47 +435,69 @@ void handlerHelp(char* param)
 
 void setup()
 {
+ 
   // YX5300 Serial interface
   softserial.begin(MD_YX5300::SERIAL_BPS);
   mp3.begin();
   setCallbackMode(bUseCallback);
   setSynchMode(bUseSynch);
 
-  // command line interface
-  Console.begin(115200);
+  // Console interface from the IDE
+  Console.begin(ConsoleBaudRate);
+  // Other person's cod for async CLI into the code.
   CP.begin();
   CP.help();
 
+  pixels.begin(); //  Initializes the NeoPixel library.
+  turnPixelsSolid(10,0,10);
+  pixels.show();
 }
 
 
+
+
+
+int ringUpdateDelayCounter = 0;
+int ringUpdateDelayTrigger = 200;   // Update the display once evry 20 times through loop
 void loop()
 {
   //CP.run();
-
+  Console.print (" >");
+  Console.println (ringUpdateDelayCounter);
+  ringUpdateDelayCounter++;
+  if (ringUpdateDelayCounter >= ringUpdateDelayTrigger) {
+    ringUpdateDelayCounter = 0;
+    //Console.print (" >");
+    //Console.println (ringUpdateDelayCounter);
+    showTimeOnNeopixelRing();
+  }
 
   while (chimeCounter < Hours) {  //used to be TimesToChime
 
     Console.print ("\b\b-=");
     Console.println (isTrackFinished);
-    //delay(2000);
+    //delay(1000);
     if (isTrackFinished == true) {
-
+      turnPixelsSolid(0, 0, 0);
       Console.print ("Beginning chime #");
-        
-      //setColor(chimeCounter,20,20,20,100); //red
- 
+
+      setColor(chimeCounter,20,20,20,100); //red
+
       Console.print (chimeCounter);
       Console.print ("\n");
       Console.print ("isTrackFinished is TRUE, so STARTING A PLAY.  First setting isTrackFinished to FALSE\n");
       isTrackFinished = false;
       //delay (1000);
-     
+      turnPixelsSolid(0, 0, 0);
       mp3.playSpecific(1, 1);
-      delay(200);
+      //delay(100);
       chimeCounter++;
     }
+
+
     delay (100);
     mp3.check();
   }
+  //turnPixelsSolid(0, 0, 0);
+  //showTimeOnNeopixelRing();
 }
