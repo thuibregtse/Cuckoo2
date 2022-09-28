80a47a6b827a234047b6b00c897e4d586e2b3c35
thuibregtse <tomhuib@gmail.com>
2022-09-28 12:51:46 -0600

    stripped down MP3 cuckoo code

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,32 @@
 // Dependencies
 // MD_cmdProcessor library found at https://github.com/MajicDesigns/MD_cmdProcessor
 //
-#include <Adafruit_NeoPixel.h>
+
+#include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
+//#include <Streaming.h>      // https://github.com/janelia-arduino/Streaming
+
+//#include <Adafruit_NeoPixel.h>
 #include <MD_YX5300.h>
 #include <MD_cmdProcessor.h>
 #include <SoftwareSerial.h>
 
-#define ConsoleBaudRate 115200
-#define NeoPixelPin D5
+
+
+
+
 // Connections for soft serial interface to the YX5300 module.  Note these are "D" (data) pin numbers, not physical pin numbers
 const uint8_t MP3_RX = 0;    // connect to TX of MP3 Player module
 const uint8_t MP3_TX = 2;    // connect to RX of MP3 Player module
 
-#define NumberOfNeoPixels 24  //  This is for the 24-LED neopixel ring.  Adjust for what you have for Neopixels
+#define ConsoleBaudRate 115200
 #define USE_SOFTWARESERIAL 1   ///< Set to 1 to use SoftwareSerial library, 0 for native serial port
 #define Console   Serial   // command processor input/output stream
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 #define CMD(s) { Console.print(F("\n>")); Console.print(F(s)); Console.print(F(" ")); }
 
+//DS3232RTC myRTC;
 
-
-// Neopixel color for showing hour and 1/2 hours on the ring.  Adjust for Neopixel strip
-int HourLedColor[] = {50, 50, 50, 100};  // TODO: Should pre-define some static colors
-
-int neoPixelArray[NumberOfNeoPixels][4];  // RGBS array so we can set all NeoPixel and write all at once.
+// TODO:  Move to Display include file.  Neopixel color for showing hour and 1/2 hours on the ring.  Adjust for Neopixel strip
 
 
 // Chimecounter is used to increment chimes up until the current hour count
@@ -47,8 +50,7 @@ int Seconds = -1;
 //  We will check that there are readable audio files
 int filesOnDisk = 0;  
 
-// RGB strip has connected to NeoPixelPin.  There are other, more real-time modes, but we're saving our interrupts and timers for now.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NumberOfNeoPixels, NeoPixelPin, NEO_GRB + NEO_KHZ800);
+
 
 // Create a soft serial connection to the MP3 module
 SoftwareSerial  softserial(MP3_RX, MP3_TX);  // MP3 player serial stream for MP3 comms
@@ -68,63 +70,12 @@ bool trackError = false;
 
 char outputString[20];
 
-
-void readRTC() {
-  ;  
-  // Read the real-time clock, put results in Hours, Minutes, Seconds
-  // This gets called during the main loop.
-  // Results determine if chime or update Neopixel ring.
-  // TODO: Stub for RTC module or NTP code.
-  // RTP module will also need a "set values" routine
-}
-
-//simple function which takes values for the red, green and blue led and also
-//a delay
-void setColor(int led, int redValue, int greenValue, int blueValue, int delayValue)
-{
-  pixels.setPixelColor(led, pixels.Color(redValue, greenValue, blueValue));
-  pixels.show();
-  //delay(delayValue);
-}
-
-void showTimeOnNeopixelRing() {
-  //  Update the  neopixel ring
-  //  Values in Hours + minutes determine how many LEDs are lit, and how bright
-  //  0, 2, 4, and other even-numbered LEDs represent an hour and are fully lit
-  //  Odd-numberred LEDs represent the half-hour, and my be less fully lit or a different color
-  //  This means noon and midnight would be dark....maybe have to re-think that.
-  int i = 0;
-
-  while ( i <= Hours ) {
-    setColor(((i - 1) * 2), HourLedColor[0], HourLedColor[1], HourLedColor[2], HourLedColor[3]);
-    i++;
-  }
-  //if (Minutes >= 30)  // Dim light for each half hour.  Might be able to do this with just luminance.
-  //  setColor((((Hours - 1) * 2) + 1), (HourLedColor[0] / 2), (HourLedColor[1] / 2), (HourLedColor[2] / 2), (HourLedColor[3] / 2));
-}
-
-void turnPixelsSolid(int red, int green, int blue) {
-  int led;
-  for (led = 0; led <= NumberOfNeoPixels; led++)
-  {
-    setColor(led, red, green, blue, 10);
-  }
-}
-
 void cbResponse(const MD_YX5300::cbData *status)
 // Used to process device responses either as a library callback function
 // or called locally when not in callback mode.
 {
  
   Console.print ("Callback:");
-
-/*
-  if (bUseSynch)
-    Console.print(F("\nSync Status: "));
-  else
-    Console.print(F("\nCback status: "));
-*/
-
 
 //Console.print ("Callback data:");
 //Console.println(status->data);
@@ -165,8 +116,8 @@ void cbResponse(const MD_YX5300::cbData *status)
   }
   Console.print("\n");
 
- // Console.print(F(", 0x"));
- // Console.print(status->data, HEX);
+  //Console.print(F(", 0x"));
+  //Console.print(status->data, HEX);
 }
 
 void setCallbackMode(bool b)
@@ -193,12 +144,6 @@ char * getNum(char *cp, uint32_t &v, uint8_t base = 10)
   return (rp);
 }
 
-//  This would be different based on different displays, e.g. Neopixel, 7-segment, etc
-void initClockDisplay() {
-  pixels.begin(); //  Initializes the NeoPixel library.
-  turnPixelsSolid(10,0,10);
-  pixels.show();
-}
 
 
 void initMp3Module() {
@@ -209,24 +154,48 @@ void initMp3Module() {
   setSynchMode(bUseSynch);
 }
 
-
-void setup()
+void printDigits(int digits)
 {
-  Console.begin(ConsoleBaudRate);
-  initMp3Module();
-  initClockDisplay();
-  // Send a request to check if there are files on the SD card
-  // Result will be in filesOnDisk
-  mp3.queryFilesCount();
-  delay(1000);
+    // utility function for digital clock display: prints preceding colon and leading 0
+    Serial.print(':');
+    if(digits < 10)
+        Serial.print('0');
+    Serial.print(digits);
 }
 
 
 
+void setup()
+{
+  Console.begin(ConsoleBaudRate);
 
+  Serial.begin(115200);
+  initMp3Module();
+  /*
+  Serial << F( "\n" __FILE__ "\n" __DATE__ " " __TIME__ "\n" );
+
+  myRTC.begin();
+  delay(500);
+  
+  setSyncProvider(myRTC.get);   // the function to get the time from the RTC
+  if(timeStatus() != timeSet)
+        Serial.println("Unable to sync with the RTC");
+  else
+        Serial.println("RTC has set the system time");
+   */
+  ///
+ //// initClockDisplay();
+  // Send a request to check if there are files on the SD card
+  // Result will be in filesOnDisk
+  ////mp3.queryFilesCount();
+  ////delay(1000);
+
+
+}
 
 void loop()
 {
+
   // In the loop we'll be:
   //  1.  Checking for variables changed by callbacks.
   //      these variables are:
@@ -237,38 +206,39 @@ void loop()
   //  3.  Checking if it's time to chime, e.g. XX:00 on the clock
   //  4.  Periodically update any neopixel/7-segment or other display
 
+  // (Asynchronously) Check for status changes in the MP3 module.  Could be Track Finished, File Count, etc
 
+  Console.print("L");
+  mp3.check();
 
-// (Asynchronously) Check for status changes in the MP3 module.  Could be Track Finished, File Count, etc
-   
-//Console.print (".");
-mp3.check();
+  // Make sure we have a track to play
+  if (fileCountStatusChange == true)
+  {
+    Console.print("Number of tracks:");
+    Console.print(numberOfTracks);
+    Console.print("\n");
+    fileCountStatusChange = false;
+    delay(500);
+  }
 
-// Make sure we have a track to play
-if (fileCountStatusChange == true) {
-  Console.print ("Number of tracks:");
-  Console.print (numberOfTracks);
-  Console.print("\n");
-  fileCountStatusChange = false;
-  delay (500);
-}
+  // Can only be one type of response at a time.
 
-// Can only be one type of response at a time.
+  else if (isTrackFinished == true)
+  { // Set to true initially, and on completion of each track
+    // Console.print ("isTrackFinished:True\n");
 
-else if (isTrackFinished == true) {  // Set to true initially, and on completion of each track
-      //Console.print ("isTrackFinished:True\n");
-  
-   // Do we need to play additional chimes?
-      if (chimeCounter < Hours) {
-        chimeCounter++;
-        Console.print("CHIME ");
-        Console.println(chimeCounter);
-        Console.print("\n");
-//delay (200);
-        mp3.playSpecific(1, 1);
-        isTrackFinished = false;
-        //delay (200);
-
+    // Do we need to play additional chimes?
+    if (chimeCounter < Hours)
+    {
+      chimeCounter++;
+      Console.print("CHIME ");
+      Console.println(chimeCounter);
+      Console.print("\n");
+      // delay (200);
+      Console.print("STARTING TRACK 1,1\n");
+      mp3.playSpecific(1, 1); // Send serial request to play track. In loop, monitor isTrackFinished, which will be set by callback
+      isTrackFinished = false;
+      // delay (200);
     }
     delay (100);
   }
@@ -280,9 +250,5 @@ else if (isTrackFinished == true) {  // Set to true initially, and on completion
       delay (500);
 
   }
+    delay(100);
 }
-
-
-
-
-
